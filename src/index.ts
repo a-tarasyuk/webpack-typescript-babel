33e7a10eabb518e4507310c09dde6bf468153753
Alexander <alexander.tarasyuk@outlook.com>
2018-11-16 20:55:51 +0200

    add fork-ts-checker-webpack-plugin, update example

--- src/index.ts
+++ src/index.ts
@@ -24,5 +24,11 @@ export class Value {
   }
 }
 
-new Value().setValue(1000);
-new Value().setProps({ x: 1000, y: 1000 });
+const value = new Value();
+value.setValue(1000);
+value.getValue();
+
+const value1 = new Value()
+value1.setProps({ x: 1000, y: 1000 });
+value1.getProps();
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
-  entry: './src/index',
+  entry: path.resolve(__dirname, 'src'),
 
   output: {
     filename: 'bundle.js',
@@ -15,4 +16,8 @@ module.exports = {
   module: {
     rules: [{ test: /\.(ts|js)x?$/, loader: 'babel-loader', exclude: /node_modules/ }],
   },
+
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+  ]
 };
